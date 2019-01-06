fcc9c8d076caf19cb7bb6f16aeede9c40185ecfb
André Azevedo <andre.azev@gmail.com>
2019-01-06 09:39:30 -0300

    Update.

--- webpack.config.js
+++ webpack.config.js
@@ -15,16 +15,33 @@ var config = {
 	},
 	output: {
 		path: BUILD_DIR,
-		filename: '[name].js'
+		filename: '[name].js',
+		chunkFilename: 'vendor.js'
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					chunks: 'initial',
+					name: 'vendor',
+					test: /node_modules/,
+					enforce: true
+				},
+			}
+		}
 	},
 	module : {
-	rules : [
-		{
-		test : /\.jsx?/,
-		include : APP_DIR,
-		loader : 'babel-loader'
-		}
-	]
+		rules : [
+			{
+				test : /\.jsx/,
+				include : APP_DIR,
+				exclude : /(node_modules|vendor|bower_components)/,
+				loader : 'babel-loader',
+				options : {
+					"presets" : ["@babel/preset-env", "@babel/preset-react"],
+				}
+			}
+		]
 	}
 };
 
